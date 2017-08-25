44dbce618de1dae57b484b1d62398617b18fc696
Celso Dantas <celsodantas@gmail.com>
2017-08-24 23:45:05 -0400

    adding basic ui menus

---
+++ Assets/Scripts/ItemMenuPopulator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemMenuPopulator : MonoBehaviour {
+
+	public GameObject itemPrefabModel;
+
+	// Use this for initialization
+	void Start () {
+		GameObject newElement = GameObject.Instantiate(itemPrefabModel);
+		newElement.transform.SetParent (transform, false);
+
+		GameObject newElement2 = GameObject.Instantiate(itemPrefabModel);
+		newElement2.transform.SetParent (transform, false);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
