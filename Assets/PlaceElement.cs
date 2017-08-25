1ab1f2be28b3aafa796bd18918d57b5ebd436f44
Celso Dantas <celsodantas@gmail.com>
2017-08-24 22:37:53 -0400

    placement code simplification and asset changes

--- Assets/PlaceElement.cs
+++ Assets/PlaceElement.cs
@@ -5,15 +5,13 @@ using VRTK;
 
 public class PlaceElement : MonoBehaviour
 {
-
     public VRTK_ControllerEvents controller;
     public VRTK_ControllerEvents.ButtonAlias activationButton = VRTK_ControllerEvents.ButtonAlias.TriggerPress;
     public VRTK_CustomRaycast customRaycast;
 
-    bool isGrabbingElement = false;
-    GameObject grabbedObject = null;
-    float maximumPlacementLength = 500f;
-    float objectRotation = 0f;
+    private GameObject grabbedObject = null;
+    private float maximumPlacementLength = 500f;
+    private float objectRotation = 0f;
 
     void Start()
     {
@@ -28,30 +26,26 @@ public class PlaceElement : MonoBehaviour
 
     protected virtual void DoActivationButtonPressed(object sender, ControllerInteractionEventArgs e)
     {
-        isGrabbingElement = true;
-        grabbedObject = InstantiateAndReturnObject();
+        grabbedObject = GetSelectedObject();
     }
 
     protected virtual void DoActivationButtonReleased(object sender, ControllerInteractionEventArgs e)
     {
-        isGrabbingElement = false;
         grabbedObject = null;
     }
 
-    private GameObject InstantiateAndReturnObject()
+    private GameObject GetSelectedObject()
     {
         Object[] objects = Resources.LoadAll("Models/Furniture/", typeof(GameObject));
         return GameObject.Instantiate((GameObject)objects[0]);
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
-        if (isGrabbingElement)
+        if (grabbedObject != null)
         {
             // positioning
             Ray rayCast = new Ray(transform.position, transform.forward);
-
             LayerMask layersToIgnore = Physics.IgnoreRaycastLayer;
 
             RaycastHit pointerCollidedWith;
