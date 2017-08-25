f9bf20c67fbf5f8a6fd41ab36d30f70edd2fdaf9
Celso Dantas <celsodantas@gmail.com>
2017-08-25 08:57:10 -0400

    left hand menu

---
+++ Assets/Scripts/AttachObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttachObject : MonoBehaviour {
+
+    public GameObject attach;
+    public Vector3 rotationAdjustment;
+
+    private GameObject objectWrapper = new GameObject();
+
+	// Use this for initialization
+	void Start () {
+        attach.transform.SetParent(transform);
+        attach.transform.position = new Vector3();
+        attach.transform.rotation = Quaternion.Euler(rotationAdjustment);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        objectWrapper.transform.position = transform.position;
+    }
+}
